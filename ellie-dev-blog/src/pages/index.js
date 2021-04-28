45d6d56e536461463f657823de5047919e822123
Eleanor <e.e.j@live.co.uk>
2021-04-28 17:32:56 +0100

    add background image

--- ellie-dev-blog/src/pages/index.js
+++ ellie-dev-blog/src/pages/index.js
@@ -1,15 +1,37 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
 
+import BackgroundImage from "gatsby-background-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "banner.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <BackgroundImage
+        tag="header"
+        fluid={data.file.childImageSharp.fluid}
+        style={{ height: "100rem" }}
+      >
+        <h1>HELLO</h1>
+      </BackgroundImage>
+    </Layout>
+  )
+}
 
 export default IndexPage
